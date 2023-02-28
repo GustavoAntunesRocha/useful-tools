40a3678a8b97b39b4cb9c3eda178bd247ad12e56
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-02-28 16:07:10 -0300

    Project creation

---
+++ useful-tools-project/src/main/java/br/com/antunes/gustavo/usefultools/UsefulToolsProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.usefultools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulToolsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulToolsProjectApplication.class, args);
+	}
+
+}
---
+++ useful-tools-project/src/test/java/br/com/antunes/gustavo/usefultools/UsefulToolsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.usefultools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulToolsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
